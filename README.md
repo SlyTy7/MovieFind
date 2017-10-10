7d05da5e7e2480014d61b665075f564a666d8689
slyty7 <tyler.j.west.7@gmail.com>
2017-10-10 11:28:45 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Movie Find
+
+A web application that searches movies by a title query and returns information on the movie.
+
+## Built With
+
+  * HTML
+  * CSS
+  * JavaScript
+  * jQuery
+  * Bootstrap
+  * Bootswatch(Bootstrap Theme)
+  * Axios
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## To-Do:
+
+  * Add subheading to movie title on 'More Info' page
+  * Add multiple genres to movie
+
+## Credits
+
+API calls made from [The Movie DB](https://www.themoviedb.org/documentation/api)
---
+++ js/main.js
@@ -0,0 +1,101 @@
+$(document).ready(function(){
+
+	console.log("Page Ready");
+
+	$("#searchForm").on("submit", function(event){
+		let searchText = $("#searchText").val();
+		getMovies(searchText);
+		event.preventDefault();
+	});
+
+});
+
+//returns list of movies that closely match search query
+function getMovies(searchText){
+	axios.get("https://api.themoviedb.org/3/search/movie?api_key=e603619cc7ad76d78e846cf21cd944cf&language=en-US&query="+searchText+"&page=1&include_adult=false")
+		//function that runs if API call succeeds
+		.then(function(response){
+			console.log(response);
+			let movies = response.data.results;
+			let output = "";
+
+			//gets image, title, and link for each movie in results
+			$.each(movies, function(index, movie){
+				let image = "https://image.tmdb.org/t/p/w500"+movie.poster_path;
+				let title = movie.title;
+				let id = movie.id;
+				
+				//html formats each result
+				output += `
+					<div class="col-sm-6 col-md-3">
+						<div class="well text-center">
+							<img src="${image}" alt="Movie Poster of '${title}'" class="movie-img">
+							<h5>${title}</h5>
+							<a onclick="movieSelected('${id}')" class="btn btn-primary" href="#">Movie Details</a>
+						</div>
+					</div>
+				`;
+			});
+
+			//output all results to html page
+			$("#movies").html(output);
+
+		})
+		//function that runs if API call failed
+		.catch(function(error){
+			console.log(error);
+		});
+}
+
+function movieSelected(id){
+	sessionStorage.setItem("movieId", id);
+	window.location = "movie.html";
+	return false;
+}
+
+function getMovie(){
+	let movieId = sessionStorage.getItem("movieId");
+
+	axios.get("https://api.themoviedb.org/3/movie/"+movieId+"?api_key=e603619cc7ad76d78e846cf21cd944cf&language=en-US&append_to_response=credits")
+		//function that runs if API call succeeded
+		.then(function(response){
+			console.log(response);
+			let movie = response.data;
+			let image = "https://image.tmdb.org/t/p/w500"+movie.poster_path;
+			let releaseDate = movie.release_date.slice(0,4);
+
+			let output = `
+				<div class="row">
+					<div class="col-md-4 col-sm-6">
+						<img src="${image}" alt="Movie Poster of '${movie.title}'" class="thumbnail">
+					</div>
+					<div class="col-md-8 col-sm-6">
+						<h2>${movie.title}</h2>
+						<ul class="list-group movie-info">
+							<li class="list-group-item"><strong>Released:</strong> ${releaseDate}</li>
+							<li class="list-group-item"><strong>Genres:</strong> ${movie.genres[0].name}</li>
+							<li class="list-group-item"><strong>Director:</strong> ${movie.credits.crew[1].name}</li>
+							<li class="list-group-item"><strong>Cast:</strong> ${movie.credits.cast[0].name}, ${movie.credits.cast[1].name}, ${movie.credits.cast[2].name}</li>
+							<li class="list-group-item"><strong>Runtime:</strong> ${movie.runtime} minutes</li>
+						</ul>
+					</div>
+				</div>
+				<div class="row">
+					<div class="well">
+						<h3>Plot</h3>
+						${movie.overview}
+						<hr>
+						<a href="http://imdb.com/title/${movie.imdb_id}" target="_blank" class="btn btn-primary">View IMDB</a>
+						<a href="index.html" class="btn btn-default">Go Back To Search</a>
+					</div>
+
+				</div>
+			`;
+
+			$("#movie").html(output);
+		})
+		//function that runs if API call failed
+		.catch(function(error){
+			console.log(error);
+		});
+}
